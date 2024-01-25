994b09cea19fc2e374a52a9b8d554300559afadb
Your Name <rimonron.ad@gmail.com>
2024-01-25 13:57:15 +0600

    project setup

---
+++ src/pages/Lesson1.tsx
@@ -0,0 +1,9 @@
+const Lesson1 = () =>{
+    return (
+        <div> 
+            <h1>Lesson -1 </h1>
+        </div>
+    )
+}
+
+export default Lesson1;
---
+++ src/pages/Lesson2.tsx
@@ -0,0 +1,9 @@
+const Lesson2 = () =>{
+    return (
+        <div> 
+            <h1>Lesson -2 </h1>
+        </div>
+    )
+}
+
+export default Lesson2;
