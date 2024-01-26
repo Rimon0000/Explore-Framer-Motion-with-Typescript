e3b8e9837bff902bcb70036e0e96d413d0c14eb6
Your Name <rimonron.ad@gmail.com>
2024-01-26 12:44:26 +0600

    Gasture-Drag

--- src/pages/Lesson1.tsx
+++ src/pages/Lesson1.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 // const parent = {
 //     initial : {rotate: 0},
@@ -8,31 +9,38 @@ const parent = {
     hidden : { opacity: 0, scale: 0.9 },
     visible : { opacity: 1, scale: 1 , 
         transition : { ease: "easeInOut", duration: 2,}},
-    hover: {scale: 1.1, transition: {duration: 0.5, },},
-    tap: { scale: 1, rotate: 45 },
+    hover: {scale: 1.1, transition: {duration: 0.5 }},
+    tap: {scale: 1.1, boxShadow: "0px 10px 10px #000"},
 }
 
-// const child = {
-//     hidden : { opacity: 0, scale: 0.1 },
-//     visible : { opacity: 1, scale: 1, 
-//         transition : { ease: "easeInOut", duration: 2, delayChildren: 0.5, staggerChildren: 0.5 }},
-// }
+const child = {
+    hidden : { opacity: 0, scale: 0.1 },
+    visible : { opacity: 1, scale: 1, 
+        transition : { ease: "easeInOut", duration: 2, delayChildren: 0.5, staggerChildren: 0.5 }},
+}
 
 const Lesson1 = () =>{
+
+    const parentRef = useRef(null)
+
     return (
+        <div ref={parentRef} className="border border-red-500 size-[500px] flex justify-center items-center">
         <motion.div className="size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
         variants={parent}
         initial= "hidden"
         animate= "visible" 
         whileHover="hover"
         whileTap="tap"
-        onHoverStart={() => console.log("Hover me")}
-        onHoverEnd={() => console.log("Hover ended")}
+        drag
+        // dragSnapToOrigin
+        dragElastic = {0.7}
+        dragConstraints = {parentRef}
+        // dragConstraints = {{right: 200, left: -200, top: -200, bottom: 200}}
         // transition={{ ease: "linear", duration: 2, repeat: Infinity, repeatType: "reverse" }}
         
         > 
 
-        {/* <motion.div className="size-20 bg-cyan-400 rounded-sm" 
+        <motion.div className="size-20 bg-cyan-400 rounded-sm" 
             variants={child}></motion.div>
 
         <motion.div className="size-20 bg-cyan-400 rounded-sm"
@@ -42,9 +50,10 @@ const Lesson1 = () =>{
             variants={child}></motion.div>
         
         <motion.div className="size-20 bg-cyan-400 rounded-sm"
-            variants={child}></motion.div> */}
+            variants={child}></motion.div>
         
         </motion.div>
+        </div>
     )
 }
 
