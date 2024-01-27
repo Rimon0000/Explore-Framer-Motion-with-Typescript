9ac01246a125c456e5044f49afe344065221e798
Your Name <rimonron.ad@gmail.com>
2024-01-27 15:17:33 +0600

    Framer motion Hook- useAnimationControl()

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
-import Lesson1 from './pages/Lesson1'
+// import Lesson1 from './pages/Lesson1'
+import Lesson2 from './pages/Lesson2'
 
 function App() {
 
   return (
     <div className='main'>
       <div className='lesson1'>
-      <Lesson1></Lesson1>
+      {/* <Lesson1></Lesson1> */}
+      <Lesson2></Lesson2>
       </div>
     </div>
   )
--- src/pages/Lesson2.tsx
+++ src/pages/Lesson2.tsx
@@ -1,7 +1,46 @@
+import {motion, useAnimationControls} from "framer-motion"
+import { useEffect, useState } from "react"
+
 const Lesson2 = () =>{
+
+    const controls = useAnimationControls()
+    const [toggle, setToggle] = useState(false)
+
+    useEffect(() =>{
+        controls.start((i) => ({x: 200, transition: {delay: i * 1}}))
+
+    }, [])
+
+    // const handleToggle = () => {
+    //     setToggle(!toggle)
+    //     if(toggle){
+    //         controls.start({ x: 200 })
+    //     }
+    //     else{
+    //         controls.start({ x: -200 })
+    //     }
+    // }
+
     return (
-        <div> 
-            <h1>Lesson -2 </h1>
+        <div onMouseEnter={() => controls.stop()} 
+            onMouseLeave={() => controls.start((i) => ({x: 200, transition: {delay: i * 1}}))} 
+            className="border border-red-500 size-[500px] flex flex-col justify-center items-center"> 
+            {/* <button onClick={handleToggle} className="bg-green-500 mb-10 px-2 py-3">Forward</button> */}
+            <motion.div className="size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+            animate = {controls}
+            custom={1}
+            >
+            </motion.div>
+            <motion.div className="size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+            animate = {controls}
+            custom={2}
+            >
+            </motion.div>
+            <motion.div className="size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+            animate = {controls}
+            custom={3}
+            >
+            </motion.div>
         </div>
     )
 }
