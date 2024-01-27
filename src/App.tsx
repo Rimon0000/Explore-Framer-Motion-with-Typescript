56cae442eda496ea8af6beb8cc66dcb4c43405e5
Your Name <rimonron.ad@gmail.com>
2024-01-28 00:04:26 +0600

    Framer motion Hook- combination of useMotionValue() and useTransform()

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Lesson10 from './pages/Lesson10'
+import Lesson11 from './pages/Lesson11'
 
 function App() {
 
   return (
     <div className='w-full flex flex-col justify-center items-center'>
       {/* <div className='h-[200vh] w-full bg-red-500'></div> */}
-      <Lesson10></Lesson10>
+      <Lesson11></Lesson11>
     </div>
   )
 }
