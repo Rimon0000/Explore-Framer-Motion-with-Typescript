5a6499e33b1223a0aa107c4e8b816abb9e455378
Your Name <rimonron.ad@gmail.com>
2024-01-27 18:11:18 +0600

    Framer motion Hook- useCycle()

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 // import Lesson1 from './pages/Lesson1'
-import Lesson2 from './pages/Lesson2'
+// import Lesson2 from './pages/Lesson2' 
+import Lesson7 from './pages/Lesson7'
 
 function App() {
 
@@ -8,7 +9,8 @@ function App() {
     <div className='main'>
       <div className='lesson1'>
       {/* <Lesson1></Lesson1> */}
-      <Lesson2></Lesson2>
+      {/* <Lesson2></Lesson2> */}
+      <Lesson7></Lesson7>
       </div>
     </div>
   )
