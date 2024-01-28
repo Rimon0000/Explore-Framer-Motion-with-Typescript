503ed6599230daf5ee28a67b95d15b7b0428c5fa
Your Name <rimonron.ad@gmail.com>
2024-01-28 14:08:10 +0600

    Framer motion Hook- AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Lesson12 from './pages/Lesson12'
+import Lesson13 from './pages/Lesson13'
 
 function App() {
 
   return (
     <div className='w-full flex flex-col justify-center items-center'>
-      <div className='h-[200vh] w-full bg-red-500'></div>
-      <Lesson12></Lesson12>
+      {/* <div className='h-[200vh] w-full bg-red-500'></div> */}
+      <Lesson13></Lesson13>
     </div>
   )
 }
