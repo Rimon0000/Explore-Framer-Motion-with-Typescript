02bde390a15bda99f29cc159e4df1c30748aa258
Your Name <rimonron.ad@gmail.com>
2024-01-28 13:05:40 +0600

    Framer motion Hook- useScroll()

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Lesson11 from './pages/Lesson11'
+import Lesson12 from './pages/Lesson12'
 
 function App() {
 
   return (
     <div className='w-full flex flex-col justify-center items-center'>
-      {/* <div className='h-[200vh] w-full bg-red-500'></div> */}
-      <Lesson11></Lesson11>
+      <div className='h-[200vh] w-full bg-red-500'></div>
+      <Lesson12></Lesson12>
     </div>
   )
 }
